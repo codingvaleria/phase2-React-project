9a56b6085a39aa80175192b34b279a3b126ca2c4
codingvaleria <codingvaleria@gmail.com>
2022-10-04 21:12:17 +0300

    moved navbar component from App to Recommendation component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./Form.css";
 import React from "react";
-import NavBar from "./NavBar";
 import Reccomendation from "./Reccomendation";
 import AdminForm from "./AdminForm";
 
 function App() {
   return (
     <div>
-      <NavBar />
       {/* <Reccomendation /> */}
       <AdminForm />
     </div>
--- src/Reccomendation.js
+++ src/Reccomendation.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LocationList from "./LocationList";
 import Location from "./Location";
+import NavBar from "./NavBar";
 
 function Reccomendation() {
   return (
-    <div id="body-details">
-      <LocationList />
-      <Location />
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <div id="body-details">
+        <LocationList />
+        <Location />
+      </div>
+    </React.Fragment>
   );
 }
 
