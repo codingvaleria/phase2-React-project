5b27469f098c22cc61330fb21932d19aca976dd3
codingvaleria <codingvaleria@gmail.com>
2022-10-04 21:04:17 +0300

    returned form.js as jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./Form.css";
 import React from "react";
 import NavBar from "./NavBar";
 import Reccomendation from "./Reccomendation";
+import AdminForm from "./AdminForm";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Reccomendation />
+      {/* <Reccomendation /> */}
+      <AdminForm />
     </div>
   );
 }
