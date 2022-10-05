ca0930c9d20bb1586d3cbd5a71f31710177ca391
codingvaleria <codingvaleria@gmail.com>
2022-10-05 15:03:42 +0300

    fetch reccomendation names on inital load

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LocationList from "./LocationList";
 import Location from "./Location";
 import NavBar from "./NavBar";
 
 function Home() {
+  const [recommendations, setReccomendations] = useState([]);
+  // const [listedLocations, setListedLocations] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8002/recommendations")
+      .then((r) => r.json())
+      .then((recommendations) => setReccomendations(recommendations));
+  }, []);
+
   return (
     <React.Fragment>
       <NavBar href="/about" text="About Us" />
       <div id="body-details">
-        <LocationList />
+        <LocationList recommendations={recommendations} />
         <Location />
       </div>
     </React.Fragment>
--- src/LocationList.js
+++ src/LocationList.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-function LocationList() {
+function LocationList({ recommendations }) {
   return (
     <div id="menu">
       <div className="menu-inner">
         <h2> Recommendation </h2>
       </div>
+
       <ul id="locations" className="locations">
-        <li className="location">
-          <a href="#"> Nairobi Museum </a>
-        </li>
-        <li className="location">
-          <a href="#"> Nairobi National Park</a>
-        </li>
-        <li className="location">
-          <a href="#">Memorial Park </a>
-        </li>
+        {recommendations.map((recommendation) => {
+          return (
+            <li className="location" key={recommendation.id}>
+              {recommendation.location}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
