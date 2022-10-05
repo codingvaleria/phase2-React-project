9fa43fc3b58d349c5f80d4eaef508c79fc327836
codingvaleria <codingvaleria@gmail.com>
2022-10-05 10:10:59 +0300

    Add tbody and thead tags

--- src/Charges .js
+++ src/Charges .js
@@ -8,30 +8,35 @@ function Charges() {
         Entry Charges <span className="Ksh">(price in Ksh) </span>
       </h2>
       <table>
-        <tr>
-          <th>Type</th>
-          <th>Child </th>
-          <th>Student</th>
-          <th>Adult </th>
-        </tr>
-        <tr>
-          <td> Citizen </td>
-          <td id="citizen-child-charge"> 200 </td>
-          <td id="citizen-student-charge"> 100</td>
-          <td id="citizen-adult-charge"> 100</td>
-        </tr>
-        <tr>
-          <td> Resident</td>
-          <td id="resident-child-charge"> 200 </td>
-          <td id="resident-student-charge"> 100</td>
-          <td id="resident-adult-charge"> 100</td>
-        </tr>
-        <tr>
-          <td>Non- Resident</td>
-          <td id="non-resident-child-charge"> 200 </td>
-          <td id="non-resident-student-charge"> 100</td>
-          <td id="non-resident-adult-charge"> 100</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Child </th>
+            <th>Student</th>
+            <th>Adult </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td> Citizen </td>
+            <td id="citizen-child-charge"> 200 </td>
+            <td id="citizen-student-charge"> 100</td>
+            <td id="citizen-adult-charge"> 100</td>
+          </tr>
+          <tr>
+            <td> Resident</td>
+            <td id="resident-child-charge"> 200 </td>
+            <td id="resident-student-charge"> 100</td>
+            <td id="resident-adult-charge"> 100</td>
+          </tr>
+          <tr>
+            <td>Non- Resident</td>
+            <td id="non-resident-child-charge"> 200 </td>
+            <td id="non-resident-student-charge"> 100</td>
+            <td id="non-resident-adult-charge"> 100</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
