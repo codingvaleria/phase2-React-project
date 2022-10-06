6a823e3fe064eecf379235cd46324c8148757073
codingvaleria <codingvaleria@gmail.com>
2022-10-06 11:32:26 +0300

    Fix styling

--- src/About.js
+++ src/About.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
+import "./About.css";
 
 function About() {
   return (
     <React.Fragment>
       <NavBar href="/" text="Home" />
-      <div className="details">
+      <div className="about-details">
         <h1 id="title"> About Us</h1>
         <div id="description-container" className="description-container">
           We are your tour directory. We give you recommendations of places to
@@ -14,7 +15,7 @@ function About() {
           amenities of the place to visit.
         </div>
 
-        {/* <img src="./images/safari.jpeg"> </img> */}
+        <img src="./image/safari.jpeg" />
       </div>
     </React.Fragment>
   );
